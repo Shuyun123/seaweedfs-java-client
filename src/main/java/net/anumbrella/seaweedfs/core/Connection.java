96d49271cab2f24268d3ded6424e276d2c1f0e6f
黄洪波 <hhb12345@126.com>
2018-05-29 22:11:07 +0800

    1

--- src/main/java/net/anumbrella/seaweedfs/core/Connection.java
+++ src/main/java/net/anumbrella/seaweedfs/core/Connection.java
@@ -24,6 +24,7 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.cache.CacheConfig;
 import org.apache.http.impl.client.cache.CachingHttpClients;
